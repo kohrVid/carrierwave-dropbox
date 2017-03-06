da82160794f87927c3dbd32255e1e7bada7d2b80
kohrVid <kohrVid@zoho.com>
2017-03-06 00:47:40 +0000

    experiment

--- spec/carrierwave-uploader-base_spec.rb
+++ spec/carrierwave-uploader-base_spec.rb
@@ -11,7 +11,6 @@ describe CarrierWave::Uploader::Base do
     described_class.should respond_to(:dropbox_app_key)
     described_class.should respond_to(:dropbox_app_secret)
     described_class.should respond_to(:dropbox_access_token)
-    described_class.should respond_to(:dropbox_access_token_secret)
     described_class.should respond_to(:dropbox_user_id)
     described_class.should respond_to(:dropbox_access_type)
   end
